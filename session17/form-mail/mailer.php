9852598128e32012b7702c73f048ad36f4a44801
Daniel Johnson <dan@balancerockmedia.com>
2015-07-15 11:24:33 -0400

    edits

---
+++ session17/form-mail/mailer.php
@@ -0,0 +1,26 @@
+<?php
+
+// get form field values
+$first_name = $_POST['first_name'];
+$last_name = $_POST['last_name'];
+$email = $_POST['email'];
+$comments = $_POST['comments'];
+
+// setup email
+$to = 'dan@balancerockmedia.com';
+$subject = 'Email Form Submission';
+$message = "First Name: $first_name \n";
+$message .= "Last Name: $last_name \n";
+$message .= "Comments: $comments \n";
+$headers = "From: $email" . "\r\n" . 'Reply-To: webmaster@example.com';
+
+// echo $message;
+
+// send email
+if (mail($to, $subject, $message, $headers)) {
+    echo 'success!';
+} else {
+    echo 'fail!';
+}
+
+?>
